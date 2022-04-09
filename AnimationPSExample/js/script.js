54c8ad752543766449d3e5697275baf7c09a7b89
patryCCio <patryk.szczerbinski@o2.pl>
2022-04-09 22:01:40 +0200

    Przykladowe uzycie dla scroll'a

---
+++ AnimationPSExample/js/script.js
@@ -0,0 +1,9 @@
+import { Animation } from "../../AnimationPS/js/main.js";
+
+const liEl = document.querySelectorAll('.ul-li');
+const navbar = document.querySelector('[data-navbarps]');
+const burger = document.querySelector('[data-burgerps]');
+
+const elements = document.querySelectorAll('[data-hrefposps]');
+
+Animation.scroll('ease', 1000, 10, elements, false, null, true, liEl, true, navbar, 200, true, burger);
