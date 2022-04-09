f06d14e8ee13ef4ab24036d46aaee379f40e5197
patryCCio <patryk.szczerbinski@o2.pl>
2022-04-10 01:35:35 +0200

    Uproszczenie kodu oraz pobor informacji o elementach za pomoca datasetow!

--- AnimationPSExample/js/script.js
+++ AnimationPSExample/js/script.js
@@ -1,10 +1,3 @@
 import { Animation } from "../../AnimationPS/js/main.js";
 
-const liEl = document.querySelectorAll('.ul-li');
-const navbar = document.querySelector('[data-navbarps]');
-const burger = document.querySelector('[data-burgerps]');
-
-const elements = document.querySelectorAll('[data-hrefposps]');
-console.log(navbar);
-
-Animation.scroll('ease', 1000, 10, elements, false, null, true, liEl, true, navbar, 200, true, burger);
+Animation.scroll('ease', 1000, 10, true, true, true, 100, true);
